378861f043273359d5d590716cb66d506a6ee46e
mac <mac@appledeiMac.local>
2016-01-30 19:32:32 +0800

    add index to show info

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
 var express = require("express");
+var nunjucks = require("nunjucks");
 var time2json = require("./time2json");
 var app = express();
+
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: app
+});
+
+app.get('/', function(req, res) {
+  res.render('index.html', {app_url: process.env.APP_URL});
+});
 
 app.get('/:time', function(req, res) {
     res.end(time2json(req.params.time)+'\n');
 });
+
 app.listen(process.env.PORT, function(){
   console.log('Server start, listen on ' + process.env
   .IP + ':' + process.env.PORT);
