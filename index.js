f152f912603b0caa9e4023383db8eeff145e0218
Wenchao Hu <zjuhwc@gmail.com>
2016-02-16 23:02:56 +0800

    update format

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-var express = require("express");
-var nunjucks = require("nunjucks");
-var time2json = require("./time2json");
+var express = require('express');
+var nunjucks = require('nunjucks');
+var time2json = require('./time2json');
 var app = express();
 
 nunjucks.configure('templates', {
-    autoescape: true,
-    express: app
+  autoescape: true,
+  express: app
 });
 
 app.get('/', function(req, res) {
@@ -14,10 +14,10 @@ app.get('/', function(req, res) {
 });
 
 app.get('/:time', function(req, res) {
-    res.end(time2json(req.params.time)+'\n');
+  res.end(time2json(req.params.time) + '\n');
 });
 
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT, function() {
   console.log('Server start, listen on ' + process.env
   .IP + ':' + process.env.PORT);
 });
